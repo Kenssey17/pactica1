eb80941bdc4a3db0b3aa26bcd2ca9f400b10b38f
Kenssey17 <braxxxan@gmail.com>
2022-09-30 06:58:49 -0500

    actualizacion

--- src/main/java/org/example/Coche.java
+++ src/main/java/org/example/Coche.java
@@ -1,2 +1,18 @@
-package org.example;public class Coche {
+package org.example;
+
+public class Coche {
+    int numeroPuertas;
+
+
+
+    public void aumentarPuertas(){
+        this.numeroPuertas++;
+    }
+
+    public static void main (String[] args){
+        Coche miCoche = new Coche();
+        miCoche.aumentarPuertas();
+
+        System.out.println(miCoche.numeroPuertas);
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,17 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    var valor = suma(10, 20, 30);
+
+        System.out.println(valor);
+
+    }
+
+    public static int suma (int a, int b, int c){
+
+
+        return a + b + c;
+
     }
 }
